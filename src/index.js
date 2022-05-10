9cb973707aa1dccdba79827da187a52cf1fe9378
axel soler <99031215+AxelSoler@users.noreply.github.com>
2022-05-10 12:40:58 -0300

    functionality for refresh button

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import scores from "./modules/scores.js";
 import displayScores from './modules/display.js';
 
 const leaderboard = document.getElementById('scoresLeaderboard');
+const refreshBtn = document.getElementById('refreshBtn');
+refreshBtn.addEventListener('click', () => window.location.reload());
 
 window.onload = () => {
   displayScores(scores, leaderboard);
