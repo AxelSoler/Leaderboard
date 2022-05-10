241ac795375be1d16f637de69614ee2e454ba28e
axel soler <99031215+AxelSoler@users.noreply.github.com>
2022-05-10 20:27:11 -0300

    API post user and score

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import './index.css';
 import scores from './modules/scores.js';
 import displayScores from './modules/display.js';
+import postImput from './modules/post.js';
 
 const leaderboard = document.getElementById('scoresLeaderboard');
 const refreshBtn = document.getElementById('refreshBtn');
-refreshBtn.addEventListener('click', () => window.location.reload());
+const form = document.getElementById('addScore');
+const urlAPI = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/Zl4d7IVkemOTTVgoifqoiwjfa/scores/';
 
-window.onload = () => {
-  displayScores(scores, leaderboard);
-};
+postImput(urlAPI);
---
+++ src/modules/post.js
@@ -0,0 +1,23 @@
+const inputName = document.getElementById('inputName');
+const inputScore = document.getElementById('inputScore');
+const submitBtn = document.getElementById('submitBtn');
+const form = document.getElementById('addScore');
+
+const postImput = (url) => {
+  submitBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    fetch(url, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        user: inputName.value.trim(),
+        score: inputScore.value.trim(),
+      }),
+    })
+      .then((response) => response.json())
+      .then((json) => console.log(json));
+      form.reset();
+  });
+};
+
+export default postImput;
