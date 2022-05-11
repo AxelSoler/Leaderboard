c6debf9696331dc66d4a05af8acb13950adb2f6c
axel soler <99031215+AxelSoler@users.noreply.github.com>
2022-05-11 15:30:19 -0300

    sort scores

--- src/modules/display.js
+++ src/modules/display.js
@@ -2,6 +2,7 @@ import scores from './scores.js';
 
 const displayScores = async (url, board) => {
   const allScores = await scores(url);
+  allScores.sort((a, b) => b.score - a.score);
   allScores.forEach((element) => {
     board.innerHTML += `<div class='scoreDiv'>
       <h4>${element.user}: ${element.score}</h4>
