574afdb7e2efd72ccd717c4bdbcba9e43a6f4eff
axel soler <99031215+AxelSoler@users.noreply.github.com>
2022-05-12 16:59:46 -0300

    fix linter errors

--- src/modules/move.js
+++ src/modules/move.js
@@ -2,9 +2,9 @@ const move = () => {
   const scoreMove = document.querySelectorAll('h4');
 
   scoreMove.forEach((element) => {
-    const time = Math.floor(Math.random() * (16 - 6) / 2) + 3;
+    const time = Math.floor(Math.random() * ((16 - 6) / 2)) + 3;
     element.style.animationDuration = `${time}s`;
   });
-}
+};
 
 export default move;
